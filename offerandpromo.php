01aecaea1430f84de0660eb6b5947d3e91417c5e
osykris <osy.krisdayanti27@gmail.com>
2020-12-01 22:34:08 +0700

    third commit

---
+++ offerandpromo.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="history.css">
+
+    <title>Wood Art Furniture</title>
+</head>
+
+<body>
+
+    <a href="getStarted.php"><img src="asset/button_back.png" alt="Image" width="100px" style="position: absolute;
+    width: 24px;
+    height: 24px;
+    left: 41px;
+    top: 63px;" /></a>
+
+    <h4 style="  position: absolute;
+    width: 261px;
+    height: 51px;
+    left: 50px;
+    top: 124px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 34px;
+    line-height: 51px;
+    letter-spacing: -0.05em;
+
+    color: #000000;">Offer and promo</h4><br><br>
+    <img src="asset/cart.png" width="100px" style="position: absolute;
+    width: 113.13px;
+    height: 107.99px;
+    left: 125px;
+    top: 340px;" alt="...">
+    <h1 style="
+    position: absolute;
+    width: 320px;
+    height: 42px;
+    left: 27px;
+    top: 468px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 28px;
+    line-height: 42px;
+
+    /* identical to box height */
+    text-align: center;
+    letter-spacing: -0.02em;
+
+    color: #000000;">Ohh snap! No offers yet</h1>
+    <h2 style="position: absolute;
+    width: 233px;
+    height: 52px;
+    left: 71px;
+    top: 518px;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 17px;
+    line-height: 25px;
+    text-align: center;
+
+    color: #000000;
+
+    opacity: 0.57;">Novend Osyana doesn't have any offers yet please check again.</h2>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
---
+++ productDetail.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="productDetail.css">
+
+    <title>Wood Art Furniture</title>
+</head>
+
+<body>
+    <a href="getStarted.php"><img src="asset/button_back.png" alt="Image" width="100px" style="position: absolute;
+    width: 24px;
+    height: 24px;
+    left: 41px;
+    top: 63px;" /></a>
+
+    <a href="getStarted.php"><img src="asset/heart.png" alt="Image" width="100px" style="position: absolute;
+    width: 20px;
+    height: 20px;
+    left: 320px;
+    top: 63px;" /></a>
+
+    <a href="getStarted.php"><img src="asset/img_detail.png" alt="Image" width="100px" style="position: absolute;
+    width: 241.21px;
+    height: 241.21px;
+    left: 66px;
+    top: 94px;
+    filter: drop-shadow(0px 40px 40px rgba(0, 0, 0, 0.07));" /></a>
+
+    <a href="getStarted.php"><img src="asset/sliderDetail.png" alt="Image" width="100px" style="position: absolute;
+    width: 68px;
+    height: 8px;
+    left: 152px;
+    top: 310px;" /></a>
+
+    <h1>Anabella Corner</h1>
+
+
+    <h2>Rp. 11.372.000</h2>
+
+    <h3>Details</h3>
+
+    <h4>The Urban's simple silhouette and sink-right-in comfort make it a classic choice for the family room. Hand upholstered and assembled in the USA, it's designed with deep seats and comes with your choice of back cushion fill: 100% poly or down and feather wrapped. You won't want to get up, trust us.</h4>
+
+    <a class="btn" href="#">
+        Add to cart
+    </a>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
