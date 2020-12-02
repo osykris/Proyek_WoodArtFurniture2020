647c07c267b914795b0f03c7c818c7f0ec67cde1
osykris <osy.krisdayanti27@gmail.com>
2020-12-02 22:56:47 +0700

    fourth commit

---
+++ cart.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="cart.css">
+
+    <title>Wood Art Furniture</title>
+</head>
+
+<body>
+    
+    <a href="getStarted.php"><img src="asset/button_back.png" alt="Image" width="100px" style="position: absolute;
+    width: 24px;
+    height: 24px;
+    left: 41px;
+    top: 63px;"/></a>
+
+    <h3>Cart</h3><br><br>
+    <img src="asset/swipe.png" class="swipe" width="100px" alt="...">
+    <img src="asset/background.png" class="background" alt="...">
+    <img src="asset/gambar.png" class="gambar" alt="...">
+    <h2>Anabella Corner</h2>
+    <h1>Rp. 11.372.000</h1>
+    <a class="btn"href="#">
+        Complete order
+    </a>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
---
+++ orders.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="cart.css">
+
+    <title>Wood Art Furniture</title>
+</head>
+
+<body>
+
+    <a href="getStarted.php"><img src="asset/button_back.png" alt="Image" width="100px" style="position: absolute;
+    width: 24px;
+    height: 24px;
+    left: 41px;
+    top: 63px;" /></a>
+
+    <h3>Orders</h3><br><br>
+    <img src="asset/background.png" class="ordersbgr" alt="...">
+    <img src="asset/gambar.png" class="gambar2" alt="...">
+    <h6 style="position: absolute;
+    width: 205px;
+    height: 19px;
+    left: 120px;
+    top: 151px;
+
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 101.34%;
+
+    /* or 16px */
+    text-align: center;
+
+    color: #000000;">Anabella Corner</h6>
+    <hr style="position: absolute;
+    width: 161px;
+    height: 0px;
+    left: 162px;
+    top: 159px;
+
+    border: 1px solid #9A9A9D;">
+    <h6 style="position: absolute;
+    width: 162px;
+    height: 17px;
+    left: 162px;
+    top: 184px;
+
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 101.34%;
+
+    /* or 12px */
+
+    color: #000000;">Order Num.  122276531NFGT</h6>
+    <hr style="position: absolute;
+    width: 161px;
+    height: 0px;
+    left: 162px;
+    top: 188px;
+
+    border: 1px solid #9A9A9D;">
+    <h6 style="position: absolute;
+    width: 162px;
+    height: 17px;
+    left: 162px;
+    top: 215px;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 12px;
+    line-height: 101.34%;
+
+    /* or 12px */
+
+    color: #000000;">Status : Process</h6>
+    <h6 style="position: absolute;
+    width: 186px;
+    height: 12px;
+    left: 159px;
+    top: 237px;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 101.34%;
+
+    /* or 12px */
+    text-align: center;
+
+    color: #AD8460;">Total Pembayaran : Rp. 11.372.000</h6>
+    <a class="btn" href="#">
+        Start ordering
+    </a>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
