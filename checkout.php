d346fdc4e2a15e31daf8885d6019b04dba335737
osykris <osy.krisdayanti27@gmail.com>
2020-12-04 11:38:22 +0700

    fourth commit

---
+++ checkout.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="checkout.css">
+
+    <title>Wood Art Furniture</title>
+</head>
+
+<body>
+
+    <a href="getStarted.php"><img src="asset/button_back.png" alt="Image" width="100px" style="position: absolute;
+    width: 24px;
+    height: 24px;
+    left: 41px;
+    top: 63px;" /></a>
+
+    <h3>Checkout</h3><br><br>
+    <h1 class="dev">Delivery</h1>
+    <h1 class="address">Address details</h1>
+    <a class="change" href="#">change</a>
+    <img src="asset/delivery.png" class="background" alt="...">
+    <img src="asset/delivery.png" class="subbackground" alt="...">
+    <h1 class="username">Novend Osyana </h1>
+    <hr style="position: absolute;
+    width: 180px;
+    height: 1px;
+    left: 54px;
+    top: 284px;
+
+    background: #C4C4C4;">
+    <h1 class="detailaddress">Jl. Kenangan No. 101 RR Malang Jawa Timur</h1>
+    <hr style="position: absolute;
+    width: 180px;
+    height: 1px;
+    left: 59px;
+    top: 343px;
+
+    background: #C4C4C4;">
+    <h1 class="phone">+6281355766123</h1>
+    <h1 class="method">Delivery method.</h1>
+
+    <form method="POST" action="#">
+        <div class="formya">
+            <input  type="radio" name="option1" value="Door delivery" >  Door delivery <BR><BR><BR>
+            <input type="radio" name="option2" id="option2" value="Pickup"> Pickup <BR><BR>
+        </div>
+            <input class="btn" type="submit" name="done" value="Proceed to payment">
+    </form>
+
+    <hr style="position: absolute;
+    width: 232px;
+    height: 0px;
+    left: 84px;
+    top: 553px;
+
+    opacity: 0.3;
+    border: 0.5px solid #000000;">
+
+    <h1 class="totalname">Total</h1>
+    <h1 class="total">Rp. 11.372.000</h1>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
---
+++ profile.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="profile.css">
+
+    <title>Wood Art Furniture</title>
+</head>
+
+<body>
+
+    <a href="getStarted.php"><img src="asset/button_back.png" alt="Image" width="100px" style="position: absolute;
+    width: 24px;
+    height: 24px;
+    left: 41px;
+    top: 63px;" /></a>
+
+    <h4 style="  position: absolute;
+    width: 261px;
+    height: 51px;
+    left: 50px;
+    top: 109px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 34px;
+    line-height: 51px;
+    letter-spacing: -0.05em;
+
+    color: #000000;">My Profile</h4>
+    <h1 class="personil">Personal details</h1>
+    <a class="change" href="#">change</a>
+    <img src="asset/profile.png" alt="Image" class="profile"/>
+
+    <h1 class="name">Novend Osyana </h1>
+    <h1 class="email">Osy&Novend@gmail.com</h1>
+    <h1 class="phone">+6281355766123</h1>
+    <h1 class="alamat">Jl. Kenangan No. 101 RR Malang Jawa Timur</h1>
+
+    <img src="asset/orders.png" alt="Image" class="orders"/>
+    <img src="asset/pendingreviews.png" alt="Image" class="previews"/>
+    <img src="asset/faq.png" alt="Image" class="faq"/>
+    <img src="asset/help.png" alt="Image" class="help"/>
+    <a class="btn" href="#">
+        Update
+    </a>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
