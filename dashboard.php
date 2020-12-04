178736cebbf0a7f859f78b8975e3e47a649c753f
osykris <osy.krisdayanti27@gmail.com>
2020-12-04 14:27:36 +0700

    fifth commit

---
+++ dashboard.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="dashboard.css">
+
+    <title>Wood Art Furniture</title>
+</head>
+
+<body>
+
+    <a href="getStarted.php"><img src="asset/picture.png" alt="Image" width="170px" style=" position: absolute;
+    left: 61.35%;
+    right: -42.27%;
+    top: 18.41%;
+    bottom: 19.75%;" /></a>
+    <img src="asset/shadow.png" alt="Image" width="170px" style=" position: absolute;
+    width: 246.1px;
+    height: 531.1px;
+    left: 233.03px;
+    top: 207.2px;
+    background: url(asset/shadow.png);
+    opacity: 0.2;" />
+    <img src="asset/prof.png" alt="Image" width="200px" style=" 
+    position: absolute;
+    width: 24px;
+    height: 24px;
+    left: 38px;
+    top: 18.41%;" />
+    <a class="prof" href="profile.php"><img src="asset/fontprof.png" alt="Image" width="100px" class="prof"/></a>
+    <hr style="position: absolute;
+    width: 132px;
+    height: 0.3px;
+    left: 84px;
+    top: 184.7px;
+    background: #F4F4F8;">
+    <img src="asset/order.png" alt="Image" width="200px" style="
+    position: absolute;
+    width: 26px;
+    height: 26px;
+    left: 38px;
+    top: 239px;"/>
+    <a class="prof" href="orders.php"><img src="asset/fontorders.png" alt="Image" width="60px" class="order"/></a>
+    <hr style=" position: absolute;
+    width: 132px;
+    height: 0.3px;
+    left: 84px;
+    top: 278.7px; background: #F4F4F8;">
+    <img src="asset/offer.png" alt="Image" width="200px" style="position: absolute;
+    width: 24px;
+    height: 24px;
+    left: 38px;
+    top: 340px;" />
+     <a class="prof" href="offerandpromo.php"><img src="asset/fontoffer.png" alt="Image" width="100px" class="offer"/></a>
+    <hr style="position: absolute;
+    width: 132px;
+    height: 0.3px;
+    left: 84px;
+    top: 370.7px;
+
+    background: #F4F4F8;">
+    <img src="asset/hist.png" alt="Image" width="200px" style="position: absolute;
+    width: 24px;
+    height: 24px;
+    left: 38px;
+    top: 430px;" />
+    <a class="prof" href="history.php"><img src="asset/fonthistory.png" alt="Image" width="60px" class="history"/></a>
+    <hr style="position: absolute;
+    width: 132px;
+    height: 0.3px;
+    left: 84px;
+    top: 466.7px; background: #F4F4F8;">
+    <img src="asset/secur.png" alt="Image" width="200px" style="position: absolute;
+    width: 20px;
+    height: 19.2px;
+    left: 38px;
+    top: 530px; " />
+    <a class="prof" href="security.php"><img src="asset/fontsecur.png" alt="Image" width="100x" class="security"/></a>
+    <a class="prof" href="getStarted.php"><img src="asset/fonout.png" alt="Image" width="100px" class="out"/></a>
+    <img src="asset/arrow.png" alt="Image" width="200px" style="position: absolute;
+    width: 23px;
+    height: 14.73px;
+    left: 150px;
+    top: 750.57px;" />
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
